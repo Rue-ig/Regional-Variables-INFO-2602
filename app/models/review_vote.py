79e7cb956f2301ec36f43764e05385b937bb0198
manic-cs <rinard.guy@protonmail.com>
2026-04-14 03:30:53 -0400

    Add unique constraint to ReviewVote model

--- app/models/review_vote.py
+++ app/models/review_vote.py
@@ -1,9 +1,13 @@
 # PATH: app/models.review_vote.py
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, UniqueConstraint
 from typing import Optional
 from datetime import datetime
 
 class ReviewVote(SQLModel, table=True):
+    __table_args__ = (
+        UniqueConstraint("user_id", "review_id", name="unique_user_review_vote"),
+    )
+    
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id", index=True)
     review_id: int = Field(foreign_key="review.id", index=True)
