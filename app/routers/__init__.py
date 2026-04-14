2a34d05d788b06f2eea69608b2aa2f36c8103038
manic-cs <rinard.guy@protonmail.com>
2026-04-13 20:04:28 -0400

    Refactor imports and path handling in __init__.py
    Updated import statements and adjusted BASE_DIR and static_files path handling.

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,21 +1,25 @@
 # PATH: app/routers/__init__.py
-from fastapi import APIRouter, Path
+from pathlib import Path as FilePath
+from fastapi import APIRouter, Path as ApiPath
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from app.utilities.flash import get_flashed_messages
 from jinja2 import Environment, FileSystemLoader
 from app.config import get_settings
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = FilePath(__file__).resolve().parent.parent
+
 template_env = Environment(loader=FileSystemLoader(BASE_DIR / "templates"))
 template_env.globals['get_flashed_messages'] = get_flashed_messages
 templates = Jinja2Templates(env=template_env)
-static_files = StaticFiles(directory= BASE_DIR / "static")
+
+static_files = StaticFiles(directory=str(BASE_DIR / "static"))
 
 router = APIRouter(
     tags=["Jinja Based Endpoints"],
     include_in_schema=get_settings().env.lower() in ["dev", "development"],
 )
+
 api_router = APIRouter(tags=["API Endpoints"], prefix="/api")
 
 from . import (
